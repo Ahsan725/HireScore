1caa4fae2b02bd2b5055ee6e9e5a91cf84c1478c
Ahsan <sbaseer25@gmail.com>
2024-11-14 00:10:03 -0500

    somewhat stable build

--- job_analyzer/analyzer/context_processors.py
+++
@@ -1,9 +0,0 @@
-# analyzer/context_processors.py
-
-from .models import VisitorCount
-
-def visitor_count(request):
-    visitor_count = VisitorCount.objects.first()
-    return {
-        'visitor_count': visitor_count.count if visitor_count else 0
-    }
